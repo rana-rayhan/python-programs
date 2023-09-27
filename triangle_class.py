722320a8a512ebec4875cafc4e3d15fb606c7b82
rana-rayhan <ryanrazz00@gmail.com>
2023-09-28 00:24:19 +0300

    python-simple-program

--- triangle_class.py
+++ triangle_class.py
@@ -2,16 +2,20 @@ class Triangle:
     base = 0
     height = 0
 
+    # constructor --**
     def __init__(self, base, height):
         self.base = base
         self.height = height
 
+    # function --**
     def display(self, tName):
         area = 0.5 * self.base * self.height
         print(f"{tName} area: {area}")
 
 
+# class objects
 t1 = Triangle(10, 20)
-t1.display("T1")
 t2 = Triangle(20, 30)
+
+t1.display("T1")
 t2.display("T2")
